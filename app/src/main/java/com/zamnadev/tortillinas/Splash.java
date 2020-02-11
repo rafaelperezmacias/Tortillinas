fb6d82dccc46633278edadcef1d8149138de1b1d
RafaelPM <rafaelpemaceti25@gmail.com>
2020-02-11 17:02:29 -0600

    12

---
+++ app/src/main/java/com/zamnadev/tortillinas/Splash.java
@@ -0,0 +1,23 @@
+package com.zamnadev.tortillinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
