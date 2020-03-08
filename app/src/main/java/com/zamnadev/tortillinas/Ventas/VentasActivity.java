34d096f3e45f110849211d97d25c49a29f111096
PabloPadilla5 <pablopadillamaldonado@gmail.com>
2020-03-08 15:50:01 -0600

    ventas: Fase inicial de diseño
    Signed-off-by: PabloPadilla5 <pablopadillamaldonado@gmail.com>

--- app/src/main/java/com/zamnadev/tortillinas/Ventas/VentasActivity.java
+++ app/src/main/java/com/zamnadev/tortillinas/Ventas/VentasActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.zamnadev.tortillinas.BottomSheets.VentasBottomSheet;
 import com.zamnadev.tortillinas.R;
 
 public class VentasActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class VentasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventas);
+        FloatingActionButton fabAgregarVenta = findViewById(R.id.fab_agregar_venta);
+        fabAgregarVenta.setOnClickListener((v) -> {
+            VentasBottomSheet bottomSheet = new VentasBottomSheet();
+            bottomSheet.show(getSupportFragmentManager(), bottomSheet.getTag());
+        });
     }
 }
