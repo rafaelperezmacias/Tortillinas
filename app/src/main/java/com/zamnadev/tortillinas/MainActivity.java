06a8b48906278c97e1a7f07709571d5fb80f18ea
PabloPadilla5 <pablopadillamaldonado@gmail.com>
2020-03-11 11:44:37 -0600

    MainActivity: Limpieza de código
    Signed-off-by: PabloPadilla5 <pablopadillamaldonado@gmail.com>

--- app/src/main/java/com/zamnadev/tortillinas/MainActivity.java
+++ app/src/main/java/com/zamnadev/tortillinas/MainActivity.java
@@ -1,33 +1,28 @@
 package com.zamnadev.tortillinas;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.zamnadev.tortillinas.Clientes.ClientesActivity;
-import com.zamnadev.tortillinas.Empleados.EmpleadosActivity;
 import com.zamnadev.tortillinas.Moldes.Empleado;
 import com.zamnadev.tortillinas.Sesiones.ControlSesiones;
-import com.zamnadev.tortillinas.Sucursales.SucursalesActivity;
-import com.zamnadev.tortillinas.Ventas.VentasActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
     private DatabaseReference refEmpleado;
+
     private ValueEventListener listenerEmpleado;
 
     @Override
@@ -41,49 +36,32 @@ public class MainActivity extends AppCompatActivity {
         if (ControlSesiones.ObtenerUsuarioActivo(getApplicationContext()) != null) {
             refEmpleado = FirebaseDatabase.getInstance().getReference("Empleados")
                     .child(ControlSesiones.ObtenerUsuarioActivo(getApplicationContext()));
-
             listenerEmpleado = refEmpleado.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    Log.e("Data",dataSnapshot.toString());
+                    Log.e("Data", dataSnapshot.toString());
                     Empleado empleado = dataSnapshot.getValue(Empleado.class);
-                    switch (empleado.getTipo())
-                    {
-                        case Empleado.TIPO_ADMIN:
-                        {
-
-                        } break;
-                        case Empleado.TIPO_REPARTIDOR:
-                        {
-                            ((Button) findViewById(R.id.btnEmpleados)).setVisibility(View.GONE);
-                            ((Button) findViewById(R.id.btnSucursales)).setVisibility(View.GONE);
-                        } break;
-                        case Empleado.TIPO_MOSTRADOR:
-                        {
-                            ((Button) findViewById(R.id.btnEmpleados)).setVisibility(View.GONE);
-                            ((Button) findViewById(R.id.btnSucursales)).setVisibility(View.GONE);
-                        } break;
+                    switch (empleado.getTipo()) {
+                        case Empleado.TIPO_ADMIN: {
+                            break;
+                        }
+                        case Empleado.TIPO_REPARTIDOR: {
+                            /*((Button) findViewById(R.id.btnEmpleados)).setVisibility(View.GONE);
+                            ((Button) findViewById(R.id.btnSucursales)).setVisibility(View.GONE);*/
+                            break;
+                        }
+                        case Empleado.TIPO_MOSTRADOR: {
+                            /*((Button) findViewById(R.id.btnEmpleados)).setVisibility(View.GONE);
+                            ((Button) findViewById(R.id.btnSucursales)).setVisibility(View.GONE);*/
+                            break;
+                        }
                     }
                 }
 
                 @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
+                public void onCancelled(@NonNull DatabaseError databaseError) { }
             });
         }
-
-        ((Button) findViewById(R.id.btnClientes))
-                .setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ClientesActivity.class)));
-
-        ((Button) findViewById(R.id.btnVentas))
-                .setOnClickListener(view -> startActivity(new Intent(MainActivity.this, VentasActivity.class)));
-
-        ((Button) findViewById(R.id.btnEmpleados))
-                .setOnClickListener(view -> startActivity(new Intent(MainActivity.this, EmpleadosActivity.class)));
-
-        ((Button) findViewById(R.id.btnSucursales))
-                .setOnClickListener(view -> startActivity(new Intent(MainActivity.this, SucursalesActivity.class)));
     }
 
     @Override
@@ -97,10 +75,15 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.menuCerrarSesion) {
             ControlSesiones.EliminaUsuario(getApplicationContext());
             refEmpleado.removeEventListener(listenerEmpleado);
-            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
             return true;
         }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
     }
 }
