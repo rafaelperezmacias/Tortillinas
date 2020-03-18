e0cb0c4272507b6b253e11473ed8f61f91a83bb5
PabloPadilla5 <pablopadillamaldonado@gmail.com>
2020-03-18 13:23:30 -0600

    adaptador:productos: Revisión de código y cambio de implementación para diálogo de eliminar
    Signed-off-by: PabloPadilla5 <pablopadillamaldonado@gmail.com>

--- app/src/main/java/com/zamnadev/tortillinas/Adaptadores/AdaptadorProductos.java
+++ app/src/main/java/com/zamnadev/tortillinas/Adaptadores/AdaptadorProductos.java
@@ -1,9 +1,7 @@
 package com.zamnadev.tortillinas.Adaptadores;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -18,22 +16,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.zamnadev.tortillinas.BottomSheets.ProductosBottomSheet;
+import com.zamnadev.tortillinas.Dialogs.MessageDialog;
+import com.zamnadev.tortillinas.Dialogs.MessageDialogBuilder;
 import com.zamnadev.tortillinas.Moldes.Producto;
 import com.zamnadev.tortillinas.R;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class AdaptadorProductos extends RecyclerView.Adapter<AdaptadorProductos.ViewHolder> {
-
     private Context context;
+
     private ArrayList<Producto> productos;
+
     private boolean isEditable;
+
     private FragmentManager fragmentManager;
 
-    public AdaptadorProductos(Context context, ArrayList<Producto> productos, boolean isEditable, FragmentManager fragmentManager)
-    {
+    public AdaptadorProductos(Context context, ArrayList<Producto> productos, boolean isEditable,
+                              FragmentManager fragmentManager) {
         this.context = context;
         this.productos = productos;
         this.isEditable = isEditable;
@@ -44,51 +44,56 @@ public class AdaptadorProductos extends RecyclerView.Adapter<AdaptadorProductos.
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         if (isEditable) {
-            View view = LayoutInflater.from(context).inflate(R.layout.item_adaptador_productos,parent,false);
-            return new AdaptadorProductos.ViewHolder(view);
+            View view = LayoutInflater.from(context)
+                    .inflate(R.layout.item_adaptador_productos, parent,false);
+            return new ViewHolder(view);
         } else {
-            View view = LayoutInflater.from(context).inflate(R.layout.item_adaptador_productos_preferencial,parent,false);
-            return new AdaptadorProductos.ViewHolder(view);
+            View view = LayoutInflater.from(context)
+                    .inflate(R.layout.item_adaptador_productos_preferencial, parent,false);
+            return new ViewHolder(view);
         }
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Producto producto = productos.get(position);
-
         holder.txtNombre.setText(producto.getNombre());
         holder.txtPrecio.setText("$ " + producto.getPrecio());
-
         holder.btnOpciones.setOnClickListener(view -> {
-            PopupMenu popupMenu = new PopupMenu(context,holder.btnOpciones);
+            PopupMenu popupMenu = new PopupMenu(context, holder.btnOpciones);
             popupMenu.inflate(R.menu.menu_productos);
             popupMenu.setOnMenuItemClickListener(menuItem -> {
-                switch (menuItem.getItemId())
-                {
-                    case R.id.menuEditar:
+                switch (menuItem.getItemId()) {
+                    case R.id.menuEditar: {
                         ProductosBottomSheet bottomSheet = new ProductosBottomSheet(producto);
-                        bottomSheet.show(fragmentManager,bottomSheet.getTag());
+                        bottomSheet.show(fragmentManager, bottomSheet.getTag());
                         return true;
-                    case R.id.menuEliminar:
-                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                        builder.setTitle("Alerta")
-                                .setMessage("¿Esta seguro que desea elimanar este producto?")
-                                .setPositiveButton("Eliminar", (dialogInterface, i) -> {
-                                    DatabaseReference refEmpleado = FirebaseDatabase.getInstance().getReference("Productos")
-                                            .child(producto.getIdProducto())
-                                            .child("eliminado");
-                                    refEmpleado.setValue(true)
-                                            .addOnCompleteListener(task -> {
-                                                if (task.isSuccessful()) {
-                                                    Toast.makeText(context, "Producto eliminado con exito", Toast.LENGTH_SHORT).show();
-                                                } else {
-                                                    Toast.makeText(context, "Error, intentelo mas tarde", Toast.LENGTH_SHORT).show();
-                                                }
-                                            });
-                                })
-                                .setNegativeButton("Cancelar",null)
-                                .show();
+                    }
+                    case R.id.menuEliminar: {
+                        MessageDialog dialog = new MessageDialog(context, new MessageDialogBuilder()
+                                .setTitle("Alerta")
+                                .setMessage("¿Estás seguro de que quieres eliminar este producto?")
+                                .setPositiveButtonText("Sí, Eliminar")
+                                .setNegativeButtonText("No, cancelar")
+                        );
+                        dialog.show();
+                        dialog.setPositiveButtonListener(v -> {
+                            DatabaseReference refEmpleado = FirebaseDatabase.getInstance()
+                                    .getReference("Productos")
+                                    .child(producto.getIdProducto())
+                                    .child("eliminado");
+                            refEmpleado.setValue(true).addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(context, "Producto eliminado con exito", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(context, "Error, intentelo mas tarde", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            dialog.dismiss();
+                        });
+                        dialog.setNegativeButtonListener(v -> dialog.dismiss());
                         return true;
+                    }
                 }
                 return false;
             });
@@ -97,21 +102,19 @@ public class AdaptadorProductos extends RecyclerView.Adapter<AdaptadorProductos.
     }
 
     @Override
-    public int getItemCount() {
-        return productos.size();
-    }
+    public int getItemCount() { return productos.size(); }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
+    static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView txtNombre;
         private TextView txtPrecio;
+
         private ImageButton btnOpciones;
 
-        public ViewHolder(@NonNull View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtNombre = (TextView) itemView.findViewById(R.id.txtNombre);
-            txtPrecio = (TextView) itemView.findViewById(R.id.txtPrecio);
-            btnOpciones = (ImageButton) itemView.findViewById(R.id.btnOpciones);
+            txtNombre = itemView.findViewById(R.id.txtNombre);
+            txtPrecio = itemView.findViewById(R.id.txtPrecio);
+            btnOpciones = itemView.findViewById(R.id.btnOpciones);
         }
     }
 }
