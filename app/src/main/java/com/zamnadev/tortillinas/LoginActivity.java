ed1897af4fa6fa25929e96308ddda90a61df50e6
PabloPadilla5 <pablopadillamaldonado@gmail.com>
2020-03-22 12:22:56 -0600

    login: Revisión de código, agregado progressbar y mensaje de error
    Signed-off-by: PabloPadilla5 <pablopadillamaldonado@gmail.com>

--- app/src/main/java/com/zamnadev/tortillinas/LoginActivity.java
+++ app/src/main/java/com/zamnadev/tortillinas/LoginActivity.java
@@ -1,22 +1,20 @@
 package com.zamnadev.tortillinas;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
@@ -30,86 +28,96 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText txtPassword;
     private TextInputLayout lytPassword;
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         txtUsuario = findViewById(R.id.txtUsuario);
         lytUsuario = findViewById(R.id.lytUsuario);
         txtPassword = findViewById(R.id.txtPassword);
         lytPassword = findViewById(R.id.lytPassword);
+        progressBar = findViewById(R.id.progressbar);
+
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+
+        MaterialCardView mcvError = findViewById(R.id.card_error);
 
         //TODO Usuario y contraseña para el acceso admin , admin
 
-        ((Button) findViewById(R.id.btnIngresar))
-                .setOnClickListener((View.OnClickListener) view -> {
-                    if (!validaUsuario() | !validaPassword()) {
-                        return;
-                    }
-
-                    Query reference = FirebaseDatabase.getInstance().getReference("Cuentas")
-                            .orderByChild("usuario")
-                            .equalTo(txtUsuario.getText().toString().trim());
-
-                    reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
-                            Log.e("data",dataSnapshot.toString());
-                            if (dataSnapshot.exists()) {
-                                Query reference = FirebaseDatabase.getInstance().getReference("Cuentas")
-                                        .orderByChild("password")
-                                        .equalTo(txtPassword.getText().toString().trim());
-                                reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(@NonNull DataSnapshot data) {
-                                        if (data.exists()) {
-                                            Cuenta cuenta = null;
-                                            for (DataSnapshot snapshot : data.getChildren()) {
-                                                cuenta = snapshot.getValue(Cuenta.class);
-                                            }
-                                            ControlSesiones.IngreasarUsuario(getApplicationContext(),cuenta.getIdCuenta());
-                                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                                            finish();
-                                        } else {
-                                            Toast.makeText(LoginActivity.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
-                                        }
+        btnIngresar.setOnClickListener(view -> {
+            if (!validaUsuario() | !validaPassword()) { return; }
+            btnIngresar.setText("INICIANDO SESIÓN...");
+            btnIngresar.setEnabled(false);
+            lytUsuario.setEnabled(false);
+            lytPassword.setEnabled(false);
+            progressBar.setVisibility(View.VISIBLE);
+            Query reference = FirebaseDatabase.getInstance().getReference("Cuentas")
+                    .orderByChild("usuario")
+                    .equalTo(txtUsuario.getText().toString().trim());
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
+                    Log.e("data", dataSnapshot.toString());
+                    if (dataSnapshot.exists()) {
+                        Query reference = FirebaseDatabase.getInstance().getReference("Cuentas")
+                                .orderByChild("password")
+                                .equalTo(txtPassword.getText().toString().trim());
+                        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot data) {
+                                if (data.exists()) {
+                                    Cuenta cuenta = null;
+                                    for (DataSnapshot snapshot : data.getChildren()) {
+                                        cuenta = snapshot.getValue(Cuenta.class);
                                     }
-
-                                    @Override
-                                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                                    }
-                                });
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Usuario no registrado", Toast.LENGTH_SHORT).show();
+                                    ControlSesiones.IngreasarUsuario(getApplicationContext(), cuenta.getIdCuenta());
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish();
+                                } else {
+                                    btnIngresar.setText("INGRESAR");
+                                    btnIngresar.setEnabled(true);
+                                    lytUsuario.setEnabled(true);
+                                    lytPassword.setEnabled(true);
+                                    progressBar.setVisibility(View.GONE);
+                                    mcvError.setVisibility(View.VISIBLE);
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) { }
+                        });
+                    } else {
+                        btnIngresar.setText("INGRESAR");
+                        btnIngresar.setEnabled(true);
+                        lytUsuario.setEnabled(true);
+                        lytPassword.setEnabled(true);
+                        progressBar.setVisibility(View.GONE);
+                        mcvError.setVisibility(View.VISIBLE);
+                    }
+                }
 
-                        }
-                    });
-                });
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) { }
+            });
+        });
     }
 
     private boolean validaUsuario() {
         if (txtUsuario.getText().toString().isEmpty()) {
-            lytUsuario.setError("Rellene el campo");
+            lytUsuario.setError("Por favor, ingresa tu usuario");
             return false;
         }
-
         lytUsuario.setError(null);
         return true;
     }
 
     private boolean validaPassword() {
         if (txtPassword.getText().toString().isEmpty()) {
-            lytPassword.setError("Rellene el campo");
+            lytPassword.setError("Por favor, ingresa tu contraseña");
             return false;
         }
-
         lytPassword.setError(null);
         return true;
     }
@@ -118,7 +126,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         if (ControlSesiones.ValidaUsuarioActivo(getApplicationContext())) {
-            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
             finish();
         }
     }
